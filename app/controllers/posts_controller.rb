f32590ea457c236de57cd7539be5af44f0a421e2
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-15 14:56:38 -0700

    adds new route and view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,13 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
-  	@posts = Post.all
+	@posts = Post.all
   end
 
   def new
+  	@post = Post.new
+  	@tags = Tag.all
   end
 
   def edit
@@ -11,4 +15,9 @@ class PostsController < ApplicationController
 
   def show
   end
+
+  private
+	  def set_post
+	  	@post = Post.find_by(id: params[:id])
+	  end
 end
