23d77f9729904ff2f3741046a6a8fdad79d8f4d1
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-15 14:21:40 -0700

    includes seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+
+p1 = Post.create({
+	title: "Original Post Name 1",
+	content: "Beautiful Content",
+	author: "An Inspiring Producer of Identity Porn"
+	})
+
+p2 = Post.create({
+	title: "Original Post Name 2",
+	content: "Beautiful Content",
+	author: "An Inspiring Producer of Identity Porn"
+	})
+
+p3 = Post.create({
+	title: "Original Post Name 3",
+	content: "Beautiful Content",
+	author: "An Inspiring Producer of Identity Porn"
+	})
+
+t1 = Tag.create(name: 'this is me')
+t2 = Tag.create(name: 'this isnt me!')
+t3 = Tag.create(name: 'i aspire to be this')
+t4 = Tag.create(name: 'fb like-worthy')
+t5 = Tag.create(name: 'omg')
+
+p1.tags << t1
+p1.tags << t2
+
+p2.tags << t3
+p2.tags << t4
+p2.tags << t2
+
+p3.tags << t2
+p3.tags << t4
+p3.tags << t5
+
+
